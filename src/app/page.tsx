5f4ee9c4ed6b5582b09de0314e50121a53a96c7d
DishaSingh07 <dishumau@gmail.com>
2024-10-21 13:40:13 +0530

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button"
+import { NavigationMenuDemo } from "@/components/ui/navbar"
+
 
 export default function Home() {
   return (
     <div>
-      <Button>Click me</Button>
+      <NavigationMenuDemo/>
+      {/* <Button>Login</Button> */}
+      {/* <Button>Sign up</Button> */}
     </div>
   )
 }
